1549abd2c61634bb80f37852f49e912fdb64ef4f
Turgay <turgaj.abdullajev@gmail.com>
2023-10-01 10:47:07 +0400

    created Warrior class separated rpr and war html pages

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, redirect, url_for, flash
-from rotation import Reaper
+from reaper import Reaper
+from warrior import Warrior
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = 'secret'
 
 rpr = Reaper()
+war = Warrior()
 
 
 @app.route("/")
@@ -15,25 +17,32 @@ def index():
     return render_template('home.html', gauge=rpr.soul_gauge)
 
 
+@app.route('/reaper')
+def reaper():
+    gauge = 150
+    gauge = min(gauge, 100)
+    return render_template('reaper.html', gauge=rpr.soul_gauge)
+
+
 @app.route('/slice', methods=["POST"])
 def reaper_slice():
     rpr.slice()
     print(rpr.soul_gauge)
-    return redirect(url_for('index'))
+    return redirect(url_for('reaper'))
 
 
 @app.route('/waxing_slice', methods=['POST'])
 def reaper_waxing():
     rpr.waxing_slice()
     print(rpr.soul_gauge)
-    return redirect(url_for('index'))
+    return redirect(url_for('reaper'))
 
 
 @app.route('/infernal_slice', methods=['POST'])
 def reaper_infernal():
     rpr.infernal_slice()
     print(rpr.soul_gauge)
-    return redirect(url_for('index'))
+    return redirect(url_for('reaper'))
 
 
 @app.route('/gallows', methods=['POST'])
@@ -41,21 +50,28 @@ def reaper_gallows():
     rpr.gallows()
     print(rpr.soul_gauge)
 
-    return redirect(url_for('index'))
+    return redirect(url_for('reaper'))
 
 
 @app.route('/design', methods=['POST'])
 def reaper_design():
     rpr.deaths_design()
     print(rpr.design, "seconds")
-    return redirect(url_for('index'))
+    return redirect(url_for('reaper'))
 
 
 @app.route('/reset', methods=["POST"])
-def reset():
+def reaper_reset():
     global rpr
     rpr = Reaper()
-    return redirect(url_for('index'))
+    return redirect(url_for('reaper'))
+
+
+@app.route('/warrior')
+def warrior():
+    return render_template('warrior.html')
+
+
 
 
 if __name__ == '__main__':
---
+++ warrior.py
@@ -0,0 +1,60 @@
+class Warrior:
+
+    def __init__(self):
+        self.beast_gauge = 0
+        self.surging_tempest = 0
+        self.combo_status = 0
+
+    def heavy_swing(self):
+        self.combo_status = 1
+
+    def maim(self):
+        if self.combo_status == 1:
+            self.combo_status = 2
+            if self.beast_gauge < 90:
+                self.beast_gauge += 10
+            else:
+                self.beast_gauge = 100
+        else:
+            self.combo_status = 0
+
+    def storms_path(self):
+        if self.combo_status == 2:
+            self.combo_status = 0
+            if self.beast_gauge < 80:
+                self.beast_gauge += 20
+            else:
+                self.beast_gauge = 100
+
+        else:
+            self.combo_status = 0
+
+    def storms_eye(self):
+        if self.combo_status == 2:
+            self.combo_status = 0
+            if self.beast_gauge < 90:
+                self.beast_gauge += 10
+            else:
+                self.beast_gauge = 100
+
+            if self.surging_tempest < 30:
+                self.surging_tempest += 30
+            else:
+                self.surging_tempest = 60
+        else:
+            self.combo_status = 0
+
+
+if __name__ == '__main__':
+    w = Warrior()
+    w.heavy_swing()
+    w.maim()
+    w.storms_eye()
+    w.storms_eye()
+    w.storms_eye()
+    w.storms_path()
+    w.storms_path()
+
+    print('combo status', w.combo_status)
+    print('gauge', w.beast_gauge)
+    print('buff', w.surging_tempest, 's')
