69bc411163bcab60dfd7d2ea490f1d6f0f861c94
Turgay <turgaj.abdullajev@gmail.com>
2023-10-01 07:53:36 +0400

    created basic 1-2-3 combo and spender

---
+++ rotation.py
@@ -0,0 +1,29 @@
+class Ability:
+
+    def __init__(self):
+        self.stage = 0
+        self.gauge = 0
+
+    def slice(self):
+        self.stage = 1
+        self.gauge += 10
+
+    def waxing_slice(self):
+        if self.stage == 1:
+            self.stage = 2
+            self.gauge += 10
+        else:
+            self.stage = 0
+
+    def infernal_slice(self):
+        if self.stage == 2:
+            self.stage = 0
+            self.gauge += 10
+        else:
+            self.stage = 0
+
+    def gallows(self):
+        if self.gauge >= 50:
+            self.gauge -= 50
+        else:
+            print("Not enough gauge to use Gallows")
