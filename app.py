deeb6e8d1435275f061dd0a1684f100a5c47855b
Turgay <turgaj.abdullajev@gmail.com>
2023-10-01 08:54:20 +0400

    changed every button in form to have image

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, flash
 from rotation import Reaper
 
 app = Flask(__name__)
@@ -12,35 +12,37 @@ rpr = Reaper()
 def index():
     gauge = 150
     gauge = min(gauge, 100)
-    return render_template('home.html', gauge=rpr.gauge)
+    return render_template('home.html', gauge=rpr.soul_gauge)
 
 
 @app.route('/slice', methods=["POST"])
 def reaper_slice():
     rpr.slice()
-    print(rpr.gauge)
+    print(rpr.soul_gauge)
     return redirect(url_for('index'))
 
 
 @app.route('/waxing_slice', methods=['POST'])
 def reaper_waxing():
     rpr.waxing_slice()
-    print(rpr.gauge)
+    print(rpr.soul_gauge)
     return redirect(url_for('index'))
 
 
 @app.route('/infernal_slice', methods=['POST'])
 def reaper_infernal():
     rpr.infernal_slice()
-    print(rpr.gauge)
+    print(rpr.soul_gauge)
     return redirect(url_for('index'))
 
 
 @app.route('/gallows', methods=['POST'])
 def reaper_gallows():
     rpr.gallows()
-    print(rpr.gauge)
+    print(rpr.soul_gauge)
+
     return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- rotation.py
+++ rotation.py
@@ -2,37 +2,37 @@ class Reaper:
 
     def __init__(self):
         self.stage = 0
-        self.gauge = 0
+        self.soul_gauge = 0
 
     def slice(self):
         self.stage = 1
-        if self.gauge <= 90:
-            self.gauge += 10
-        elif self.gauge > 90:
-            self.gauge = 100
+        if self.soul_gauge <= 90:
+            self.soul_gauge += 10
+        elif self.soul_gauge > 90:
+            self.soul_gauge = 100
 
     def waxing_slice(self):
         if self.stage == 1:
             self.stage = 2
-            if self.gauge <= 90:
-                self.gauge += 10
-            elif self.gauge > 90:
-                self.gauge = 100
+            if self.soul_gauge <= 90:
+                self.soul_gauge += 10
+            elif self.soul_gauge > 90:
+                self.soul_gauge = 100
         else:
             self.stage = 0
 
     def infernal_slice(self):
         if self.stage == 2:
             self.stage = 0
-            if self.gauge <= 90:
-                self.gauge += 10
-            elif self.gauge > 90:
-                self.gauge = 100
+            if self.soul_gauge <= 90:
+                self.soul_gauge += 10
+            elif self.soul_gauge > 90:
+                self.soul_gauge = 100
         else:
             self.stage = 0
 
     def gallows(self):
-        if self.gauge >= 50:
-            self.gauge -= 50
+        if self.soul_gauge >= 50:
+            self.soul_gauge -= 50
         else:
-            print("Not enough gauge to use Gallows")
+            print("Not enough Soul gauge to use Gallows")
