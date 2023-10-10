05be043b3634473dc5a17040e66523bb5cf55d62
Turgay <turgaj.abdullajev@gmail.com>
2023-10-10 15:43:56 +0400

    created paladin class

---
+++ paladin.py
@@ -0,0 +1,51 @@
+class Paladin:
+
+    def __init__(self) -> None:
+        pass
+
+    def fast_blade(self):
+        pass
+
+    def riot_blade(self):
+        pass
+
+    def royal_authority(self):
+        pass
+
+    def holy_spirit(self):
+        pass
+
+    def atonement(self):
+        pass
+
+    def goring_blade(self):
+        pass
+
+    def circle_of_scorn(self):
+        pass
+
+    def expiacion(self):
+        pass
+
+    def intervene(self):
+        pass
+
+    def confetior(self):
+        pass
+
+    def blade_of_faith(self):
+        pass
+
+    def blade_of_truth(self):
+        pass
+
+    def blade_of_valor(self):
+        pass
+
+    def requiescat(self):
+        pass
+
+    def fight_or_flight(self):
+        pass
+
+    
