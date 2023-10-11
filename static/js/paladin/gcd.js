54ed8e17dcd716be5fe7a18bfc85e5b2d43330a2
Turgay <turgaj.abdullajev@gmail.com>
2023-10-11 13:26:28 +0400

    added gcd circle indicator

--- static/js/paladin/gcd.js
+++ static/js/paladin/gcd.js
@@ -1,5 +1,5 @@
 let isOnCooldown = false;
-
+let cooldownDuration = pldGcd * 1000; // Calculate the cooldown duration
 
 function performAction(button) {
     if (isOnCooldown) {
@@ -10,8 +10,40 @@ function performAction(button) {
 
     isOnCooldown = true;
 
+    const cooldownInterval = 100; // Update every 1 second
+    let remainingTime = cooldownDuration;
+
+ // Reset the animation
+    const circle = document.querySelector(".progress-circle");
+    circle.classList.add("reset-animation");
+    setTimeout(() => {
+        circle.classList.remove("reset-animation");
+    }, 0); // Delay needed to trigger the reset
+
+    // Function to update the countdown display
+    function updateCountdownDisplay() {
+        const countdownElement = document.querySelector("#cooldown-countdown");
+        countdownElement.textContent = (remainingTime / 1000).toFixed(1);
+
+        if (remainingTime <= 0) {
+            clearInterval(countdownIntervalId);
+            countdownElement.textContent = "0.0";
+        }
+
+        remainingTime -= cooldownInterval;
+    }
+
+    const countdownIntervalId = setInterval(updateCountdownDisplay, cooldownInterval);
+
     setTimeout(() => {
         isOnCooldown = false;
         button.disabled = false;
-    }, pldGcd * 1000);
+        clearInterval(countdownIntervalId);
+        clearCountdownDisplay();
+    }, cooldownDuration);
+}
+
+// Function to clear the countdown display
+function clearCountdownDisplay() {
+    document.querySelector("#cooldown-countdown").textContent = "";
 }
