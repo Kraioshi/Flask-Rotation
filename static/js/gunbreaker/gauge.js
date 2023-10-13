7c7d5262b9bf759cf9d0b7036c245861a665b288
Turgay <turgaj.abdullajev@gmail.com>
2023-10-13 23:05:35 +0400

    added gauge.js

---
+++ static/js/gunbreaker/gauge.js
@@ -0,0 +1,44 @@
+// First
+
+function updateCartridgeOne(cartridgeValue) {
+    var $cartridgeOneEmpty = $("#gnb_cartridge_one_empty");
+    var $cartridgeOneFull = $("#gnb_cartridge_one_full");
+
+    if (cartridgeValue > 0) {
+        $cartridgeOneEmpty.hide();
+        $cartridgeOneFull.show();
+    } else {
+        $cartridgeOneEmpty.show();
+        $cartridgeOneFull.hide();
+    }
+}
+
+// Second
+
+function updateCartridgeTwo(cartridgeValue) {
+    var $cartridgeTwoEmpty = $("#gnb_cartridge_two_empty");
+    var $cartridgeTwoFull = $("#gnb_cartridge_two_full");
+
+    if (cartridgeValue > 1) {
+        $cartridgeTwoEmpty.hide();
+        $cartridgeTwoFull.show();
+    } else {
+        $cartridgeTwoEmpty.show();
+        $cartridgeTwoFull.hide();
+    }
+}
+
+// Third
+
+function updateCartridgeThree(cartridgeValue) {
+    var $cartridgeThreeEmpty = $("#gnb_cartridge_three_empty");
+    var $cartridgeThreeFull = $("#gnb_cartridge_three_full");
+
+    if (cartridgeValue > 2) {
+        $cartridgeThreeEmpty.hide();
+        $cartridgeThreeFull.show();
+    } else {
+        $cartridgeThreeEmpty.show();
+        $cartridgeThreeFull.hide();
+    }
+}
