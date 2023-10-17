59e4f45a352d99a2edcdcf13ec77b64789c4fe1c
Turgay <turgaj.abdullajev@gmail.com>
2023-10-17 21:25:51 +0400

    created dragoon ogcds

--- dragoon.py
+++ dragoon.py
@@ -6,6 +6,9 @@ class Dragoon:
     def true_thrust(self):
         pass
 
+    def raiden_thrust(self):
+        pass
+
     def vorpal_thrust(self):
         pass
 
@@ -22,4 +25,40 @@ class Dragoon:
         pass
 
     def wheeling_thrust(self):
+        pass
+
+    def spineshatter_dive(self):
+        pass
+
+    def dragonfire_dive(self):
+        pass
+
+    def lance_charge(self):
+        pass
+
+    def battle_litany(self):
+        pass
+
+    def life_surge(self):
+        pass
+
+    def geirskogul(self):
+        pass
+
+    def nastrond(self):
+        pass
+
+    def dragon_sight(self):
+        pass
+
+    def high_jump(self):
+        pass
+
+    def mirage_dive(self):
+        pass
+
+    def stardiver(self):
+        pass
+
+    def wyrmwind_thrust(self):
         pass
