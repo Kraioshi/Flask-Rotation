8e71b494d7e6b5d8ca7a7ccddc32c176641fb028
Turgay <turgaj.abdullajev@gmail.com>
2023-10-17 21:22:45 +0400

    created dragoon class with gcds

---
+++ dragoon.py
@@ -0,0 +1,25 @@
+class Dragoon:
+
+    def __init__(self):
+        pass
+
+    def true_thrust(self):
+        pass
+
+    def vorpal_thrust(self):
+        pass
+
+    def heavens_thrust(self):
+        pass
+
+    def disembowel(self):
+        pass
+
+    def chaotic_spring(self):
+        pass
+
+    def fang_and_claw(self):
+        pass
+
+    def wheeling_thrust(self):
+        pass
